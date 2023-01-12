f618d3739eae22ad4d7fa66ae25e5d18fcb819ab
Basant kumar Hota <basahota@users.noreply.github.com>
2023-01-12 14:21:19 +0530

    Add files via upload

---
+++ src/main/java/com/javatechie/OauthExampleApplication.java
@@ -0,0 +1,23 @@
+package com.javatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@RestController
+public class OauthExampleApplication {
+
+    @GetMapping
+    public String getUser(Principal principal) {
+        return principal.getName() + " Welcome !";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthExampleApplication.class, args);
+    }
+
+}
